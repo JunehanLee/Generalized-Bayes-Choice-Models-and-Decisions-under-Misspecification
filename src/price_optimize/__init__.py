cdf3f44627d9ffbc8eb6fec2f29dd3b30d3e6b0c
JunehanLee <30474870+JunehanLee@users.noreply.github.com>
2026-03-31 12:54:13 +0100

    Update __init__.py

--- src/price_optimize/__init__.py
+++ src/price_optimize/__init__.py
@@ -1,7 +1,7 @@
 from .coefficients import sample_random_coefficients
 from .dgp import generate_multinomial_dgp, export_dataframe_to_csv
 from .mnl_estimation import fit_hier_bayes_mnl, predict_hier_bayes_mnl
-
+from .profit import softmax,compute_mnl_probabilities_with_outside, expected_objective_for_target_product_mnl, optimize_single_product_price_grid_mnl,compute_observed_objective_for_target_product,make_constant_price_schedule,compute_counterfactual_objective_from_generated_df,simulate_counterfactual_objective_via_true_dgp,run_single_product_pricing_experiment
 
 __all__ = [
     "sample_random_coefficients",
@@ -9,4 +9,13 @@ __all__ = [
     "export_dataframe_to_csv",
     "fit_hier_bayes_mnl",
     "predict_hier_bayes_mnl",
+    "softmax",
+    "compute_mnl_probabilities_with_outside", 
+    "expected_objective_for_target_product_mnl",
+    "optimize_single_product_price_grid_mnl",
+    "compute_observed_objective_for_target_product",
+    "make_constant_price_schedule",
+    "compute_counterfactual_objective_from_generated_df",
+    "simulate_counterfactual_objective_via_true_dgp"
+    ,"run_single_product_pricing_experiment"
 ]
