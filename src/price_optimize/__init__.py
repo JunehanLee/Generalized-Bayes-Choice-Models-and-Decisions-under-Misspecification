0d57cd68b047c3b4b1e460becc856d22060d2475
JunehanLee <30474870+JunehanLee@users.noreply.github.com>
2026-03-31 13:25:51 +0100

    Update __init__.py

--- src/price_optimize/__init__.py
+++ src/price_optimize/__init__.py
@@ -1,7 +1,7 @@
 from .coefficients import sample_random_coefficients
 from .dgp import generate_multinomial_dgp, export_dataframe_to_csv
 from .mnl_estimation import fit_hier_bayes_mnl, predict_hier_bayes_mnl
-from .profit import PeriodOptimizationResult,PricingExperimentResult,    compute_choice_probs_mnl,    compute_expected_revenue_A,    optimize_price_A_t,    extract_price_path,    compute_observed_revenue_A,    optimize_price_path_A,    build_cf_price_schedule,    compute_cf_revenue_A,    simulate_cf_revenue,    run_pricing_experiment,
+from .profit import PeriodOptimizationResult,PricingExperimentResult,    compute_choice_probs_mnl,    compute_expected_revenue_A,    optimize_price_A_t,    extract_price_path,    compute_observed_revenue_A,    optimize_price_path_A,    build_cf_price_schedule,    compute_cf_revenue_A,    simulate_cf_revenue,    run_pricing_experiment
 
 __all__ = [
     "sample_random_coefficients",
