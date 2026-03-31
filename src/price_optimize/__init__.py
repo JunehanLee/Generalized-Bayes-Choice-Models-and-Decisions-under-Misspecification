93e2c3c3d6cc6734c710815ea6a97833cc3ed7ea
JunehanLee <30474870+JunehanLee@users.noreply.github.com>
2026-03-31 12:48:43 +0100

    Add blank line for improved readability in __init__.py

--- src/price_optimize/__init__.py
+++ src/price_optimize/__init__.py
@@ -2,6 +2,7 @@ from .coefficients import sample_random_coefficients
 from .dgp import generate_multinomial_dgp, export_dataframe_to_csv
 from .mnl_estimation import fit_hier_bayes_mnl, predict_hier_bayes_mnl
 
+
 __all__ = [
     "sample_random_coefficients",
     "generate_multinomial_dgp",
