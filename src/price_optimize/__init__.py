b8341205778060251c4437d37b071701dda6bc09
JunehanLee <30474870+JunehanLee@users.noreply.github.com>
2026-03-31 13:16:04 +0100

    Update __init__.py

--- src/price_optimize/__init__.py
+++ src/price_optimize/__init__.py
@@ -1,21 +1,36 @@
 from .coefficients import sample_random_coefficients
 from .dgp import generate_multinomial_dgp, export_dataframe_to_csv
 from .mnl_estimation import fit_hier_bayes_mnl, predict_hier_bayes_mnl
-from .profit import softmax,compute_mnl_probabilities_with_outside, expected_objective_for_target_product_mnl, optimize_single_product_price_grid_mnl,compute_observed_objective_for_target_product,make_constant_price_schedule,compute_counterfactual_objective_from_generated_df,simulate_counterfactual_objective_via_true_dgp,run_single_product_pricing_experiment
-
+from .profit import (
+    PeriodOptimizationResult,
+    PricingExperimentResult,
+    compute_choice_probs_mnl,
+    compute_expected_revenue_A,
+    optimize_price_A_t,
+    extract_price_path,
+    compute_observed_revenue_A,
+    optimize_price_path_A,
+    build_cf_price_schedule,
+    compute_cf_revenue_A,
+    simulate_cf_revenue,
+    run_pricing_experiment,
+)
 __all__ = [
     "sample_random_coefficients",
     "generate_multinomial_dgp",
     "export_dataframe_to_csv",
     "fit_hier_bayes_mnl",
     "predict_hier_bayes_mnl",
-    "softmax",
-    "compute_mnl_probabilities_with_outside", 
-    "expected_objective_for_target_product_mnl",
-    "optimize_single_product_price_grid_mnl",
-    "compute_observed_objective_for_target_product",
-    "make_constant_price_schedule",
-    "compute_counterfactual_objective_from_generated_df",
-    "simulate_counterfactual_objective_via_true_dgp"
-    ,"run_single_product_pricing_experiment"
+    "PeriodOptimizationResult",
+    "PricingExperimentResult",
+    "compute_choice_probs_mnl",
+    "compute_expected_revenue_A",
+    "optimize_price_A_t",
+    "extract_price_path",
+    "compute_observed_revenue_A",
+    "optimize_price_path_A",
+    "build_cf_price_schedule",
+    "compute_cf_revenue_A",
+    "simulate_cf_revenue",
+    "run_pricing_experiment",
 ]
